c69d4d06eff77fd6999fdd85fdf527d00743ca1b
AldairAndres <77022598+AldairAndres@users.noreply.github.com>
2025-07-22 06:47:08 -0500

    Delete Ejercicio Practico Devsu directory

--- Ejercicio Practico Devsu/cloud/infraestructura_con_terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {                                # Configuración del backend remoto del estado.
-  backend "azurerm" {                      # Se guarda el tfstate en Azure Blob Storage.
-    resource_group_name  = "rg-tfstate"    # Resource Group donde vive la Storage Account que contiene el estado.
-    storage_account_name = "sttfstate12345"# Nombre único de la Storage Account (sin mayúsculas ni guiones).
-    container_name       = "tfstate"       # Contenedor Blob donde se almacena el archivo de estado.
-    key                  = "infra-cloud.tfstate" # Nombre del blob que representará el estado de esta infra.
-  }
-}
--- Ejercicio Practico Devsu/cloud/infraestructura_con_terraform/providers.tf
+++
@@ -1,9 +0,0 @@
-###########################################################
-# providers.tf – Inicialización de providers
-###########################################################
-
-provider "azurerm" {                          # Provider de Azure
-  features {}                                  # Requerido por azurerm
-}
-
-provider "random" {}    
